3d77fac01c9a8074d7a7230d0012941763245178
Rex Holmes <holmesrex515@gmail.com>
2023-05-18 03:25:17 +0900

    serves frontend with express.static instead of indv. endpoints

--- server.js
+++ server.js
@@ -4,19 +4,7 @@ const app = express()
 
 app.use(express.json())
 
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-})
-
-app.get('/styles', (req, res) => {
-    res.sendFile(__dirname + '/public/styles.css')
-})
-
-app.get('/js', (req, res) => {
-    res.sendFile(__dirname + '/public/main.js')
-})
+app.use(express.static(__dirname + '/public'))
 
 app.listen(4000, () => {
     console.log('up on 4000')
